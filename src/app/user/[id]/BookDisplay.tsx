9261d273d3f585a27ce224782cf621dc3eb0afab
Noah Clark <noahammon00@gmail.com>
2023-05-24 17:35:25 -0600

    added dependency to useeffect and deleted console log

--- src/app/user/[id]/BookDisplay.tsx
+++ src/app/user/[id]/BookDisplay.tsx
@@ -1,24 +1,24 @@
 "use client";
 import { useBookContext } from "@/app/context/bookstore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const BookDisplay = () => {
   const bookCtx = useBookContext();
-  console.log(bookCtx.currBook);
+  const [dispBook, setDispBook] = useState()
 
   useEffect(() => {
     axios
       .get(`/api/book/${bookCtx.currBook}`)
       .then(res => {
-        console.log(res.data)
+        setDispBook(res.data)
       })
       .catch((err) => console.error(err));
-  }, []);
+  }, [bookCtx.currBook]);
   return (
     <>
       {/*@ts-ignore */}
-      <div>{bookCtx.currBook.title}</div>
+      <div>{dispBook?.userBook.title}</div>
       <div></div>
       <div></div>
     </>
