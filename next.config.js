961ea4325ef68ad94987d9460dc0c294b797e2dc
Noah Clark <noahammon00@gmail.com>
2023-06-04 15:26:14 -0600

    added next config for prisma

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async rewrites() {
+    // Generate Prisma Client during Next.js build
+    const { execSync } = require('child_process');
+    execSync('npx prisma generate');
+
+    return [];
+  },
 }
 
 module.exports = nextConfig
